a8f2e9c8fe5b65559d4896074434a8a14726c0ff
Jose Alberto Alvarez <josealvarezhuerta@me.com>
2020-05-08 16:31:48 -0500

    No bonus

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+import datetime as dt
 
 
 #################################################
@@ -20,6 +21,12 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+# Collect date variable to be used later
+session = Session(engine)
+last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+last_date_str = str(last_date[0]).split("-")
+last_date_dt = dt.date(int(last_date_str[0]),int(last_date_str[1]),int(last_date_str[2]))
+session.close()
 #################################################
 # Flask Setup
 #################################################
@@ -36,19 +43,15 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/YYYY-MM-DD<br/>"
+        f"/api/v1.0/YYYY-MM-DD/YYYY-MM-DD<br/>"
     )
 #################### precipitation Page ####################
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    last_date_str = str(last_date[0]).split("-")
-    last_date_dt = dt.date(int(last_date_str[0]),int(last_date_str[1]),int(last_date_str[2]))
-    last_date_dt
     results = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date <= last_date_dt).\
     filter(Measurement.date >= (last_date_dt-dt.timedelta(days=365))).all()
@@ -65,7 +68,7 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
-    results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).\
+    results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation)
     session.close()
     stations = []
     for station, name, latitude, longitude, altitude in results:
@@ -80,26 +83,71 @@ def stations():
     return jsonify(stations)
 #################### Temperature Obsdervations Page ####################
 
-@app.route("/api/v1.0/<start>")
-def tobs_one_date(start):
+@app.route("/api/v1.0/tobs")
+def tobs():
     session = Session(engine)
-    tobs_last12 = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
-    filter(Measurement.date >= start).all()
-    tobs_m_m_a = []
+    results = session.query(Station.station, Station.name, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Station.station == Measurement.station).\
+        filter(Measurement.date <= last_date_dt).\
+        filter(Measurement.date >= (last_date_dt-dt.timedelta(days=365))).\
+        group_by(Station.id).\
+        order_by(func.count(Station.station).desc()).limit(1).all()
+    session.close()
+    tobs = []
+    for station, name, min, max, avg in results:
+        tobs_dict = {}
+        tobs_dict["station"] = station
+        tobs_dict["name"] = name
+        tobs_dict["min"] = min
+        tobs_dict["max"] = max
+        tobs_dict["avg"] = avg
+        tobs.append(tobs_dict)    
+    return jsonify(tobs)
+
+#################### Query start Page ####################
+
+
+@app.route("/api/v1.0/<start>")
+def tobs_query(start):
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    session.close()
+    tobs_q1 = []
     for min, max, avg in results:
-        tobs_mma_dict = {}
-        tobs_mma_dict["min"] = min
-        tobs_mma_dict["max"] = max
-        tobs_mma_dict["avg"] = avg
-        tobs_m_m_a.append(tobs_dict)    
-    return jsonify(tobs_m_m_a)
+        tobsq1_dict = {}
+        tobsq1_dict["min"] = min
+        tobsq1_dict["max"] = max
+        tobsq1_dict["avg"] = avg
+        tobs_q1.append(tobsq1_dict)    
+    
+        if start <= last_date[0]:
+           return jsonify(tobs_q1) 
 
-#################### precipitation Page ####################
+    return jsonify({"error": f"There is no data for {start}. Data is avalailable up to {last_date[0]}"})
 
 
+#################### Query start end Page ####################
 
+@app.route("/api/v1.0/<start>/<end>")
+def tobs_query2(start, end):
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    session.close()
+    tobs_q2 = []
+    for min, max, avg in results:
+        tobsq2_dict = {}
+        tobsq2_dict["min"] = min
+        tobsq2_dict["max"] = max
+        tobsq2_dict["avg"] = avg
+        tobs_q2.append(tobsq2_dict)    
+    
+        if start <= last_date[0]:
+           return jsonify(tobs_q2) 
 
-
+    return jsonify({"error": f"There is no data for {start}. Data is avalailable up to {last_date[0]}"})
 
 
 if __name__ == "__main__":
